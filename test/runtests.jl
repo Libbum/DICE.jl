fc4beafe091e08bb8022e36300f1f778da63cc88
Tim DuBois <tim@neophilus.net>
2018-06-06 16:59:43 +0200

    Add quick test case for Vanilla

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,9 +1,12 @@
 using DICE
+using JuMP
 @static if VERSION < v"0.7.0-DEV.2005"
     using Base.Test
 else
     using Test
 end
 
-# write your own tests here
-@test 1 == 2
+dice = vanilla_2013R();
+solve(dice.model)
+
+@test getvalue(dice.UTILITY) == 2690.2447324365826
