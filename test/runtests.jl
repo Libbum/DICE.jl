5197c5e5e78b52d72ae194ff8978007c758c95bd
Tim DuBois <tim@neophilus.net>
2018-12-12 16:18:48 +0100

    Try adding manifest

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,11 +2,7 @@ using DICE
 using JuMP
 using Ipopt
 
-@static if VERSION < v"0.7.0-DEV.2005"
-    using Base.Test
-else
-    using Test
-end
+using Test
 
 DICE.@base mutable struct TestType{T}
     one
