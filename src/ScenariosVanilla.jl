a3719b273a57d2d1d5c09206873d09d3c77965b6
Tim DuBois <tim@neophilus.net>
2019-08-02 15:33:04 +0200

    Need to use the parameter

--- src/ScenariosVanilla.jl
+++ src/ScenariosVanilla.jl
@@ -1,9 +1,13 @@
 function assign_scenario(s::BasePriceScenario, config::VanillaOptions, params::VanillaParameters)
-    bound_cprice(params.cpricebase)
+    cprice_ubound = bound_cprice(params.cpricebase)
+    cprice_ubound[1] = params.cpricebase[1];
+    cprice_ubound
 end
 
 function assign_scenario(s::OptimalPriceScenario, config::VanillaOptions, params::VanillaParameters)
-    bound_cprice(fill(Inf, config.N))
+    cprice_ubound = bound_cprice(fill(Inf, config.N))
+    cprice_ubound[1] = params.cpricebase[1];
+    cprice_ubound
 end
 
 function assign_scenario(s::Scenario, config::VanillaOptions, params::VanillaParameters)
@@ -11,7 +15,6 @@ function assign_scenario(s::Scenario, config::VanillaOptions, params::VanillaPar
 end
 
 function bound_cprice(cprice_ubound::Array{Float64,1})
-    cprice_ubound[1] = params.cpricebase[1];
     # Warning: If parameters are changed, the next equation might make base case infeasible.
     # If so, reduce tnopol so that we don't run out of resources.
     for i in 2:config.N
